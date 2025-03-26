6b1d9a937ef5f4b2f234fab539fcd4ee5c3014da
lexa <lexagorbunov14@gmail.com>
2025-03-26 12:18:52 +0300

    XB-13: mocked dialog ID

--- skillsharing/pages/Dialogs/api/Dialogs.tsx
+++ skillsharing/pages/Dialogs/api/Dialogs.tsx
@@ -1,9 +1,13 @@
+import axios from "axios";
 import User from "../../../entity/User/User";
 import { DialogItem } from "../../../widgets/Dialog/ui/DialogTypes";
+import { BACK_URL } from "../../../shared/Consts/NetworkConsts";
+
+const CHAT_URL = '/chat';
 
 const dialogsMock: DialogItem[] = [
     {
-        id: User.getUserID(),
+        id: "67e3b3869a36154096b4bbeb",
         name: "Michael Portman",
         avatar: "/Dialogs/mate.png",
         time: 0,
@@ -24,4 +28,12 @@ export async function GetDialogs(userID: string, callback: (dialogsData: DialogI
     })).then((dialogsData) => {
         callback(dialogsData as DialogItem[]);
     })
+
+    // const {status, data} = await axios.get(BACK_URL + CHAT_URL + `/${companionID}`);
+
+    // console.log(status, data);
+
+    // if (status === 200) {
+    //     callback(data?.messages as IMessage[]);
+    // }
 }
