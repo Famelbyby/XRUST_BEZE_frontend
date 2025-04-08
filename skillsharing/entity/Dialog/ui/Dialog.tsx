0b52b047c36fdda1330f678f34ba68e96e3fa2da
Alex <lexagorbunov14@gmail.com>
2025-04-08 21:09:44 +0300

    XB-16: some appearance

--- skillsharing/entity/Dialog/ui/Dialog.tsx
+++ skillsharing/entity/Dialog/ui/Dialog.tsx
@@ -1,16 +1,15 @@
-import { useNavigate } from "react-router";
+import { Link } from "react-router";
 import { DialogProps } from "./DialogTypes";
 import React from "react";
 import { FormatHoursMinutes } from "../../../shared/Functions/FormatDate";
 import { ProfileType } from "../../../pages/Profile/ui/ProfileTypes";
-import { Skill } from "../../../widgets/ProfileLeftColumn/ProfileLeftColumnTypes";
 import { useSelector } from "react-redux";
 import { AppState } from "../../../app/AppStore";
 import './Dialog.scss'
 import { AVATAR_URL } from "../../../shared/Consts/URLS";
+import { Skill } from "../../../shared/Consts/Interfaces";
 
 const Dialog: React.FC<DialogProps> = ({dialog}) => {
-    const navigateTo = useNavigate();
     const {user} = useSelector((state: AppState) => state.user);
 
     let companion: ProfileType | undefined = undefined;
@@ -32,70 +31,70 @@ const Dialog: React.FC<DialogProps> = ({dialog}) => {
     }
 
     return (
-        <div className="dialog" onClick={() => {
-            if (dialog !== undefined) {
-                navigateTo(`/chat/${companion?.id}`)};
-            }
-        }>
-            <div className="dialog-user">
-                {dialog !== undefined && 
-                    <img className="dialog-user__avatar" src={AVATAR_URL + companion?.avatar} alt="" />
-                }
-                {dialog === undefined && 
-                    <div className="dialog-user__avatar dialog-user__avatar-mock">
-                        <div className="dialog-user__avatar-spinner">
-                        </div>
-                    </div>
-                }
-                <div className="dialog-user-info">
+        <Link to={`/chat/${companion?.id}`}>
+            <div className="dialog" >
+                <div className="dialog-user">
+                    {dialog !== undefined && 
+                        <Link to={`/profile/${companion?.id}`}>
+                            <img className="dialog-user__avatar" src={AVATAR_URL + companion?.avatar} alt="" />
+                        </Link>  
+                    }
                     {dialog === undefined && 
-                        <div className="dialog-user-info__name dialog-user-info__name-mock">
-                            <div className="dialog-user-info__name-spinner">
+                        <div className="dialog-user__avatar dialog-user__avatar-mock">
+                            <div className="dialog-user__avatar-spinner">
                             </div>
                         </div>
                     }
-                    {dialog !== undefined && 
-                        <div className="dialog-user-info__name">
-                            {companion?.username}
-                        </div>
-                    }
-                    {dialog === undefined &&
-                        <div className="dialog-user-info__last-message dialog-user-info__last-message-mock">
-                            <div className="dialog-user-info__last-message-spinner">
+                    <div className="dialog-user-info">
+                        {dialog === undefined && 
+                            <div className="dialog-user-info__name dialog-user-info__name-mock">
+                                <div className="dialog-user-info__name-spinner">
+                                </div>
                             </div>
-                        </div>
-                    }
-                    {dialog !== undefined && 
-                        <span className="dialog-user-info__last-message">
-                            {dialog.last_message!.payload}
-                        </span>
-                    }
-                    
-                    <div className="dialog-user-info__message-time">
-                        {dialog !== undefined && 
-                            <>
-                                {FormatHoursMinutes(new Date(dialog.last_message!.created_at))}
-                            </>
                         }
-                    </div>
-                </div>
-            </div>
-            <div className="dialog-info">
-                {dialog !== undefined && 
-                    <div className="dialog-info__tags">
-                        {dialogTags}
-                    </div>
-                }
-                {dialog === undefined && 
-                    <div className="dialog-info__tags dialog-info__tags-mock">
-                        <div className="dialog-info__tags-spinner">
+                        {dialog !== undefined && 
+                            <div className="dialog-user-info__name">
+                                {companion?.username}
+                            </div>
+                        }
+                        {dialog === undefined &&
+                            <div className="dialog-user-info__last-message dialog-user-info__last-message-mock">
+                                <div className="dialog-user-info__last-message-spinner">
+                                </div>
+                            </div>
+                        }
+                        {dialog !== undefined && 
+                            <span className="dialog-user-info__last-message">
+                                {dialog.last_message!.payload}
+                            </span>
+                        }
+                        
+                        <div className="dialog-user-info__message-time">
+                            {dialog !== undefined && 
+                                <>
+                                    {FormatHoursMinutes(new Date(dialog.last_message!.created_at))}
+                                </>
+                            }
                         </div>
                     </div>
-                }
-                <div className="dialog-info__seen">
+                </div>
+                <div className="dialog-info">
+                    {dialog !== undefined && 
+                        <div className="dialog-info__tags">
+                            {dialogTags}
+                        </div>
+                    }
+                    {dialog === undefined && 
+                        <div className="dialog-info__tags dialog-info__tags-mock">
+                            <div className="dialog-info__tags-spinner">
+                            </div>
+                        </div>
+                    }
+                    <div className="dialog-info__seen">
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 };
 
