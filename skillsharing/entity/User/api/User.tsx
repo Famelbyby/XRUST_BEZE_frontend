2d6b14761faecc9ef6b7fce38942ecb39ed3a226
Alex <lexagorbunov14@gmail.com>
2025-04-16 12:05:43 +0300

    XB-20: fix linter errors

--- skillsharing/entity/User/api/User.tsx
+++ skillsharing/entity/User/api/User.tsx
@@ -7,55 +7,55 @@ import { CODE_NOT_AUTHED } from '../../../shared/Consts/Codes';
 
 // const notAuthedUserMock: ProfileType | undefined = undefined;
 
-const userMock: ProfileType = {
-    id: localStorage.getItem('user-id') || "67ed4e0a66ab0aab711f8476",
-    username: 'Shkaf Unichtojitel',
-    avatar: '/Profile/avatar.png',
-    bio: '[!i for i in [‘Красивый’, ‘Умный’, ‘Обаятельный’, ‘Спортсмен’]]',
-    skills_to_learn: [
-        {
-            name: 'Figma',
-            description: '',
-            level: 'intermediate',
-        },
-        {
-            name: 'Nginx',
-            description: '',
-            level: 'beginner',
-        },
-        {
-            name: 'Docker',
-            description: '',
-            level: 'advanced',
-        },
-        {
-            name: 'JavaScript',
-            description: '',
-            level: 'advanced',
-        },
-    ],
-    skills_to_share: [
-        {
-            name: 'Kafka',
-            description: '',
-            level: 'beginner',
-        },
-        {
-            name: 'Networks',
-            description: '',
-            level: 'advanced',
-        },
-    ],
-    email: "ok",
-    created_at: "2025-01-03T00:00:00Z",
-    updated_at: "2025-01-03T00:00:00Z",
-    last_active_at: "2025-01-03T00:00:00Z",
-    preferred_format: "voice",
-    hrefs: [
-        'mail.ru',
-        'github.com',
-    ]
-}
+// const userMock: ProfileType = {
+//     id: localStorage.getItem('user-id') || "67ed4e0a66ab0aab711f8476",
+//     username: 'Shkaf Unichtojitel',
+//     avatar: '/Profile/avatar.png',
+//     bio: '[!i for i in [‘Красивый’, ‘Умный’, ‘Обаятельный’, ‘Спортсмен’]]',
+//     skills_to_learn: [
+//         {
+//             name: 'Figma',
+//             description: '',
+//             level: 'intermediate',
+//         },
+//         {
+//             name: 'Nginx',
+//             description: '',
+//             level: 'beginner',
+//         },
+//         {
+//             name: 'Docker',
+//             description: '',
+//             level: 'advanced',
+//         },
+//         {
+//             name: 'JavaScript',
+//             description: '',
+//             level: 'advanced',
+//         },
+//     ],
+//     skills_to_share: [
+//         {
+//             name: 'Kafka',
+//             description: '',
+//             level: 'beginner',
+//         },
+//         {
+//             name: 'Networks',
+//             description: '',
+//             level: 'advanced',
+//         },
+//     ],
+//     email: "ok",
+//     created_at: "2025-01-03T00:00:00Z",
+//     updated_at: "2025-01-03T00:00:00Z",
+//     last_active_at: "2025-01-03T00:00:00Z",
+//     preferred_format: "voice",
+//     hrefs: [
+//         'mail.ru',
+//         'github.com',
+//     ]
+// }
 
 export const GetUserByCookie = createAsyncThunk(
     'users/getUserByCookie',
@@ -90,6 +90,6 @@ export const GetUserByCookie = createAsyncThunk(
             error = "no session found";//error = response.data;
         })
 
-        return {error, user: data as ProfileType, status: 200};
+        return {error, user: data as ProfileType, status};
     }
 );
