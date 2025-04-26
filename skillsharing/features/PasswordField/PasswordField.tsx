c6a6e2eec69353c400a341dd3178b4138fcc6e77
Alex <lexagorbunov14@gmail.com>
2025-04-26 20:58:43 +0300

    XB-24: fix password field input

--- skillsharing/features/PasswordField/PasswordField.tsx
+++ skillsharing/features/PasswordField/PasswordField.tsx
@@ -14,8 +14,8 @@ const TextField: React.FC<PasswordFieldPropTypes<string>> = ({title, onChangingF
     return (
         <div className="password-field">
             {title}
-            <div className={"password-field-content" + (error ? " password-field-content_errored": "")}>
-                <input type={isHidden ? "password" : "text"} className="password-field-content__input" placeholder="" value={value} autoComplete="off" onChange={(event) => {
+            <div className={"password-field-content"}>
+                <input type={isHidden ? "password" : "text"} className={"password-field-content__input" + (error ? " password-field-content__input_errored": "")} placeholder="" value={value} autoComplete="off" onChange={(event) => {
                     onChangingField(event.target.value);
                 }}/>
                 <img className="password-field__eye-img" src={"/AuthPage/" + (isHidden ? "" : "closed-") + "eye.png"} alt={isHidden ? "Show password" : "Hide password"} onClick={toggleIsHidden}/>
