a961931331d68b069842fa3101cb11a939721c84
Alex <lexagorbunov14@gmail.com>
2025-04-29 14:21:42 +0300

    XB-24: make mobile materials page

--- skillsharing/entity/Material/Material.tsx
+++ skillsharing/entity/Material/Material.tsx
@@ -23,37 +23,41 @@ const Material: React.FC<MaterialItem> = (material) => {
                 </div>
             </div>
             <div className="material-footer">
-                <div className="material-footer-time">
-                    <img className="material-footer-time__img" src='/shared/clock.png' alt="" />
-                    {FormatRelativeTimeInPastInDays(new Date(material.created * SECOND_IN_MILLISECONDS))}
+                <div className="material-footer-left">
+                    <div className="material-footer-time">
+                        <img className="material-footer-time__img" src='/shared/clock.png' alt="" />
+                        {FormatRelativeTimeInPastInDays(new Date(material.created * SECOND_IN_MILLISECONDS))}
+                    </div>
+                    {material.author !== undefined && 
+                        <div className="material-footer-author" onClick={(event) => {
+                            event.stopPropagation();
+
+                            navigateTo(`/profile/${material.author_id}`);
+                        }}>
+                            <img className="material-footer-author__avatar-img" src={AVATAR_URL + material.author.avatar} alt="" />
+                            {material.author.username}
+                        </div>
+                    }
                 </div>
-                {material.author !== undefined && 
-                    <div className="material-footer-author" onClick={(event) => {
+                <div className="material-footer-right">
+                    <a href={MATERIALS_URL + '/' + material.filename} download={material.name} onClick={(event) => {
+                        event.stopPropagation();
+                    }}>
+                        <div className="material-footer-download">
+                            скачать
+                            <img className="material-footer-download__img" src="/UserMaterialsPage/download.png" alt="" />
+                        </div>
+                    </a>
+                    <div className="material-footer-share" onClick={(event) => {
                         event.stopPropagation();
 
-                        navigateTo(`/profile/${material.author_id}`);
+                        navigator.clipboard.writeText(`https://skill-sharing.ru/materials/${material.id}`);
+
+                        dispatch(setIsCopied(true));
                     }}>
-                        <img className="material-footer-author__avatar-img" src={AVATAR_URL + material.author.avatar} alt="" />
-                        {material.author.username}
+                        поделиться
+                        <img className="material-footer-share__img" src="/UserMaterialsPage/share.png" alt="" />
                     </div>
-                }
-                <a href={MATERIALS_URL + '/' + material.filename} download={material.name} onClick={(event) => {
-                    event.stopPropagation();
-                }}>
-                    <div className="material-footer-download">
-                        скачать
-                        <img className="material-footer-download__img" src="/UserMaterialsPage/download.png" alt="" />
-                    </div>
-                </a>
-                <div className="material-footer-share" onClick={(event) => {
-                    event.stopPropagation();
-
-                    navigator.clipboard.writeText(`https://skill-sharing.ru/materials/${material.id}`);
-
-                    dispatch(setIsCopied(true));
-                }}>
-                    поделиться
-                    <img className="material-footer-share__img" src="/UserMaterialsPage/share.png" alt="" />
                 </div>
             </div>
         </div>
