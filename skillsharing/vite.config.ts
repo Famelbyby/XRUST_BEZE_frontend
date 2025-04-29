ffbf945a2c6f6e0dadc13a971465518b07e15132
Alex <lexagorbunov14@gmail.com>
2025-04-29 16:59:35 +0300

    XB-24: add PWA config

--- skillsharing/vite.config.ts
+++ skillsharing/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  manifest: {
+    name: 'Skill Sharing',
+    short_name: 'Skill Sharing',
+    description: 'Education place',
+    icons: [
+      {
+        src: '/shared/parrot2.jpg',
+        type: 'image/jpg',
+      },
+    ],
+    theme_color: '#000000',
+    background_color: '#FFFFFF',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 })
