090333599d80d79d1e53fe5753eb9954241b033e
Alex <lexagorbunov14@gmail.com>
2025-05-02 13:39:36 +0300

    XB-25: add skills to profile page

--- skillsharing/widgets/ProfileMobileContent/ProfileMobileContent.tsx
+++ skillsharing/widgets/ProfileMobileContent/ProfileMobileContent.tsx
@@ -3,29 +3,33 @@ import { useSelector } from 'react-redux';
 import { AppState } from '../../app/AppStore';
 import './ProfileMobileContent.scss';
 import { Link } from 'react-router';
+import { CommunicationFormat, Skill } from '../../shared/Consts/Interfaces';
+import { CapitalizeString } from '../../shared/Functions/FormatStrings';
+import { PREFERRED_FORMAT_TRANSLATION } from '../../shared/Consts/Translations';
 
 const ProfileMobileContent: React.FC = () => {
-    const { user } = useSelector((state: AppState) => state.profile);
+    const { user: profile } = useSelector((state: AppState) => state.profile);
+    const { user: myUser } = useSelector((state: AppState) => state.user);
 
     return (
         <div className="profile-mobile-content">
             <div className="profile-mobile-content-description">
                 О себе
                 <div className="profile-mobile-content-description__field">
-                    {user === undefined && (
+                    {profile === undefined && (
                         <div className="profile-mobile-content-description__field-mock">
                             <div className="profile-mobile-content-description__field-spinner"></div>
                         </div>
                     )}
-                    {user !== undefined && (user.bio || '*стрекот сверчков*')}
+                    {profile !== undefined && (profile.bio || '*стрекот сверчков*')}
                 </div>
             </div>
-            {user !== undefined && (
+            {profile !== undefined && (
                 <div className="profile-mobile-content-hrefs">
                     Ссылки
-                    {user.hrefs && user.hrefs.length > 0 && (
+                    {profile.hrefs && profile.hrefs.length > 0 && (
                         <div className="profile-mobile-content-hrefs-examples">
-                            {user.hrefs.map((href: string, index: number) => {
+                            {profile.hrefs.map((href: string, index: number) => {
                                 const isMatched = !!href.match(/http(s)*:\/\/(.)+\.(.)+/);
 
                                 return (
@@ -49,15 +53,79 @@ const ProfileMobileContent: React.FC = () => {
                             })}
                         </div>
                     )}
-                    {(user.hrefs === null || (user.hrefs && user.hrefs.length === 0)) && (
+                    {(profile.hrefs === null || (profile.hrefs && profile.hrefs.length === 0)) && (
                         <div className="profile-mobile-content-hrefs__no-hrefs">Тут пусто</div>
                     )}
+                </div>
+            )}
+            <div className="profile-mobile-content-tags">
+                <div className="profile-mobile-content-to-share">
+                    <div className="profile-mobile-content-tags-header">
+                        <div className="profile-mobile-content-tags-header__title">
+                            Навыки
+                            <img
+                                className="profile-mobile-content-tags-header__img"
+                                title="Пользователь делится следующими навыками"
+                                src="/shared/question.png"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div className="profile-mobile-content-tags-array">
+                        {profile !== undefined &&
+                            profile.skills_to_share.map((skill: Skill) => {
+                                return (
+                                    <div
+                                        className={`profile-mobile-content-tags-array__tag profile-mobile-content-tags-array__tag_${skill.level}`}
+                                        title={`${CapitalizeString(skill.level)}`}
+                                        key={skill.name}
+                                    >
+                                        {skill.name}
+                                    </div>
+                                );
+                            })}
+                    </div>
+                </div>
+                {myUser?.id === profile?.id && (
+                    <div className="profile-mobile-content-to-learn">
+                        <div className="profile-mobile-content-to-learn-header">
+                            <div className="profile-mobile-content-to-learn-header__title">
+                                Я хочу изучить
+                            </div>
+                        </div>
+                        <div className="profile-mobile-content-to-learn-array">
+                            {profile !== undefined &&
+                                profile.skills_to_learn.map((skill: Skill) => {
+                                    return (
+                                        <div
+                                            className={`profile-mobile-content-to-learn-array__tag profile-mobile-content-to-learn-array__tag_${skill.level}`}
+                                            title={`${CapitalizeString(skill.level)}`}
+                                            key={skill.name}
+                                        >
+                                            {skill.name}
+                                        </div>
+                                    );
+                                })}
+                        </div>
+                    </div>
+                )}
+            </div>
+            {profile !== undefined && (
+                <div className="profile-preferred-format">
+                    Предпочитает общаться
+                    <div className="profile-preferred-format__example">
+                        {
+                            PREFERRED_FORMAT_TRANSLATION[
+                                profile.preferred_format as CommunicationFormat
+                            ]
+                        }
+                    </div>
                 </div>
             )}
             <div className="profile-mobile-content-materials">
                 Учебные материалы
                 <Link
-                    to={`/profile-materials/${user?.id}`}
+                    to={`/profile-materials/${profile?.id}`}
                     aria-label="Перейти к учебным материалам"
                 >
                     <div className="profile-mobile-content-materials-go-page">
