06124c9068724f97d261b5e8488642966880a0c0
Alex <lexagorbunov14@gmail.com>
2025-06-10 20:03:38 +0300

    XB-26: delete pwa

--- skillsharing/vite.config.ts
+++ skillsharing/vite.config.ts
@@ -1,37 +1,37 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+//import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
-const manifestForPlugin: Partial<VitePWAOptions> = {
-    registerType: 'autoUpdate',
-    manifest: {
-        name: 'Skill Sharing',
-        short_name: 'Skill Sharing',
-        description: 'Education place',
-        icons: [
-            {
-                src: '/shared/parrot.png',
-                sizes: '192x192',
-                type: 'image/png',
-                purpose: 'maskable',
-            },
-        ],
-        theme_color: '#FFFFFF',
-        background_color: '#FFFFFF',
-        display: 'fullscreen',
-        scope: 'https://skill-sharing.ru/',
-        start_url: 'https://skill-sharing.ru/',
-        orientation: 'portrait',
-    },
-    workbox: {
-        cleanupOutdatedCaches: true,
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}'],
-    },
-};
+// const manifestForPlugin: Partial<VitePWAOptions> = {
+//     registerType: 'autoUpdate',
+//     manifest: {
+//         name: 'Skill Sharing',
+//         short_name: 'Skill Sharing',
+//         description: 'Education place',
+//         icons: [
+//             {
+//                 src: '/shared/parrot.png',
+//                 sizes: '192x192',
+//                 type: 'image/png',
+//                 purpose: 'maskable',
+//             },
+//         ],
+//         theme_color: '#FFFFFF',
+//         background_color: '#FFFFFF',
+//         display: 'fullscreen',
+//         scope: 'https://skill-sharing.ru/',
+//         start_url: 'https://skill-sharing.ru/',
+//         orientation: 'portrait',
+//     },
+//     workbox: {
+//         cleanupOutdatedCaches: true,
+//         globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}'],
+//     },
+// };
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), VitePWA(manifestForPlugin)],
+    plugins: [react()],
     build: {
         rollupOptions: {
             output: {
