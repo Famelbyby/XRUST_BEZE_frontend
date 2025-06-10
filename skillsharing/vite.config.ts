b1fa614d7000ed1df4e2b645009400980224a4c7
Alex <lexagorbunov14@gmail.com>
2025-06-10 19:20:09 +0300

    XB-26: fix static caching by sw

--- skillsharing/vite.config.ts
+++ skillsharing/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
+interface NormalURL {
+    pathname: string;
+}
+
 const manifestForPlugin: Partial<VitePWAOptions> = {
     registerType: 'autoUpdate',
     manifest: {
@@ -26,6 +30,12 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
     workbox: {
         cleanupOutdatedCaches: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2,pdf,docx}'],
+        runtimeCaching: [
+            {
+                urlPattern: ({ url }) => !(url as NormalURL).pathname.startsWith('/api/v1/static'),
+                handler: 'NetworkFirst',
+            },
+        ],
     },
 };
 
